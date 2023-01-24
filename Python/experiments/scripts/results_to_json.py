496f1645774e7a0b724160aadb168c71944646ba
YasBenAll <yassinbenallal@hotmail.com>
2023-01-24 14:38:05 +0100

    Added raw data of our results

--- Python/experiments/scripts/results_to_json.py
+++ Python/experiments/scripts/results_to_json.py
@@ -1,26 +1,33 @@
 import json
 import argparse
 import pandas as pd
+import os
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--filename', type=str, required=True)
-args = parser.parse_args()
-
+results = os.listdir("results")
 
 if __name__ == '__main__':
-    params = pd.read_hdf(args.filename, key='task_parameters')
-    results = pd.read_hdf(args.filename, key='results') # If this throws "KeyError: 'No object named results in the file'", your code didn't run properly
+    for result in results:
+        if os.path.isdir(os.path.join("results", result)):
+            # print(os.listdir(os.path.join("results", result)))
+            for filename in os.listdir(os.path.join("results", result)):
+                if filename.endswith(".h5"):
+                    if "incomplete" not in filename:
+                        name = filename[:-3]
+                        filename = os.path.join("results", result, filename)
+                        print(filename)
 
-    # extract acceptance rate
-    joint = results.merge(params, how="left", left_on="tid", right_index=True)
-    NSF = joint[["name", "n_train", "original_nsf"]].groupby(by=["name", "n_train"]).mean().reset_index()
+                        params = pd.read_hdf(filename, key='task_parameters')
+                        results = pd.read_hdf(filename, key='results') # If this throws "KeyError: 'No object named results in the file'", your code didn't run properly
 
-    # extract means and standard errors
-    solution_found = joint.loc[joint.original_nsf == False]
-    means = solution_found[["name", "n_train", "original_acc", "antagonist_acc", "original_g", "antagonist_g"]].groupby(by=["name", "n_train"]).mean().add_suffix("_mean").reset_index()
-    ses = solution_found[["name", "n_train", "original_acc", "antagonist_acc", "original_g", "antagonist_g"]].groupby(by=["name", "n_train"]).sem().add_suffix("_se").reset_index()
+                        # extract acceptance rate
+                        joint = results.merge(params, how="left", left_on="tid", right_index=True)
+                        NSF = joint[["name", "n_train", "original_nsf"]].groupby(by=["name", "n_train"]).mean().reset_index()
 
-    # merge
-    aggregated = NSF.merge(means, how="left", on=["name", "n_train"]).merge(ses, how="left", on=["name", "n_train"])
-    aggregated.to_json(args.filename[:-2] + "json", indent=4, orient="records")
+                        # extract means and standard errors
+                        solution_found = joint.loc[joint.original_nsf == False]
+                        means = solution_found[["name", "n_train", "original_acc", "antagonist_acc", "original_g", "antagonist_g"]].groupby(by=["name", "n_train"]).mean().add_suffix("_mean").reset_index()
+                        ses = solution_found[["name", "n_train", "original_acc", "antagonist_acc", "original_g", "antagonist_g"]].groupby(by=["name", "n_train"]).sem().add_suffix("_se").reset_index()
+
+                        # merge
+                        aggregated = NSF.merge(means, how="left", on=["name", "n_train"]).merge(ses, how="left", on=["name", "n_train"])
+                        aggregated.to_json(os.path.join("results",result,name) + ".json", indent=4, orient="records")
